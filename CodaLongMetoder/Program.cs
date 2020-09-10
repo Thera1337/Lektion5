1933b123fa6129e7f940c1f2103253186734a57f
Thera1337 <70898199+Thera1337@users.noreply.github.com>
2020-09-10 12:32:56 +0200

    Add project files.

---
+++ CodaLongMetoder/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace CodaLongMetoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InfoText();
+            Addera(9,11);
+            int result = Subtrahera(25,13);
+            Console.WriteLine(result);
+
+            Console.WriteLine($"Belopp med moms: {AmountIncludingMoms(100)}");
+
+        }
+
+        private static double AmountIncludingMoms(int v)
+        {
+            return v + AdderaMoms(v);
+        }
+
+        private static double AdderaMoms(int v)
+        {
+            return v * 0.25;
+        }
+
+        private static int Subtrahera(int v1, int v2)
+        {
+            int r = v1 - v2;
+            return r;
+        }
+
+        private static void Addera(int v1, int v2)
+        {
+            Console.WriteLine(v1 + v2);
+        }
+
+        private static void InfoText()
+        {
+            Console.WriteLine("En text...");
+        }
+    }
+}
